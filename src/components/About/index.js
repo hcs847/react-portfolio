ea6bc383c6e9d0d979c8e1b6ece9a2e03b72bf23
Hila Caspi <caspi.home@gmail.com>
2021-06-05 19:47:53 -0700

    update about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,16 +8,18 @@ const About = () => {
                 <img className='profile-photo' src={profilePhoto} />
                 <h1>About me</h1>
             </div>
-            <p>I’ve always had an affinity for problem solving, as my journey took me through Financial Statements
-            to Business Systems analysis and process improvements; It presented many learning opportunities,
-            challenges and rewarding achievements but something was missing. Getting exposed to database querying and
-            automation tools scripting has opened my eyes to even greater challenges and possibilities.
+            <p>I’ve always had an affinity for problem solving, as my journey took me through
+            Financial Systems analysis and process improvements;
+            It presented many learning opportunities, challenges and rewarding achievements
+            but something was missing. Getting exposed to database querying and automation tools
+            scripting has opened my eyes to even greater challenges and possibilities.
             </p>
-            <p>With the pandemic, some unexpected positive outcomes came along, enabling me to dedicate the extra time
-            to enhancing and perfecting my coding skills via multiple online platforms and a Coding BootCamp through Berkeley.
-            Over the past year, I’ve been focused on Javascript Full Stack Web Design, React components and
-            Algorithms solving skills and now I’m ready to SHIFT GEARS and utilize full web development stack to
-            take websites to the next level.
+            <p>With the pandemic, some unexpected positive outcomes came along, enabling me to dedicate
+            the extra time to enhancing and perfecting my coding skills as a self taught developer.
+            Focusing on the MERN Stack, in addition to improving Algorithms solving skills and
+            Data structures,  I’ve been enjoying working with Javascript, React and GraphQL to
+            create user focused web apps. As a result, I’ve been able to SHIFT GEARS and utilize
+            full web development stack to take websites to the next level.
             </p>
         </section>
 
